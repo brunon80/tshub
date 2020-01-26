b3b01e4b573b5251ea728470013302ee22abbae7
brunon80 <bruno_rocha2008@hotmail.com>
2020-01-26 20:10:41 -0300

    add enableScreens

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import RootNav from './src/navigators/RootNav'
+import { enableScreens } from 'react-native-screens';
+
+enableScreens()
 
 const App: React.FC = () => {
     return <RootNav />
